2953c35381f376cf450abfb2e22dbfb1e7b51d16
Marina Kosolap <70973716+Liveently@users.noreply.github.com>
2023-10-31 15:59:37 +0700

    Update Server.java

--- lab2_TCP/Server/Server.java
+++ lab2_TCP/Server/Server.java
@@ -1,1 +1,38 @@
- 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.err.println("a number of port required");
+            System.exit(1);
+        }
+        int port = Integer.parseInt(args[0]);
+
+        ServerSocket serverSocket = null;
+        int numCon = 0;
+
+        try {
+            serverSocket = new ServerSocket(port);
+            System.out.println("Server started");
+            while (true) {
+                Socket clientSocket = serverSocket.accept(); //ожидает подключение клиента
+                System.out.println("Client accepted");
+                new Thread(new Connection(clientSocket)).start();  //новый поток для каждого клиента
+                numCon++;
+            }
+        }catch(IOException e) {
+            System.err.println(e.getMessage());
+            System.exit(1);
+        }finally {
+            try{
+                assert serverSocket != null;
+                serverSocket.close();
+            }catch (IOException e){
+                System.err.println(e.getMessage());
+                System.exit(1);
+            }
+        }
+    }
+}
