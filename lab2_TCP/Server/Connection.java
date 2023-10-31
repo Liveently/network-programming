d6b5237018216b930f2598d49379270025affc4e
Marina Kosolap <70973716+Liveently@users.noreply.github.com>
2023-10-31 16:29:08 +0700

    Create Connection.java

---
+++ lab2_TCP/Server/Connection.java
@@ -0,0 +1,61 @@
+import java.io.*;
+import java.net.Socket;
+import java.time.LocalTime;
+
+public class Connection implements Runnable {
+    private static int totalNumber;
+    private final int curNumber;
+    private final Socket clientSocket;
+
+    public Connection(Socket socket) {
+        curNumber = totalNumber++;
+        clientSocket = socket;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Client #" + curNumber + ": " + clientSocket.getInetAddress() + " connected on port " + clientSocket.getPort());
+        try (ObjectOutputStream oos = new ObjectOutputStream(clientSocket.getOutputStream());
+             ObjectInputStream in = new ObjectInputStream(clientSocket.getInputStream())) {
+            String fileName = (String) in.readObject();
+            int fileLength = in.readInt();
+            File file = new File("uploads\\" + fileName);
+
+            int totalReadLength = 0;
+            int partlyReadLength = 0;
+            int buffSize = 1024;
+            byte[] buff = new byte[buffSize];
+            LocalTime timeLast = LocalTime.now();
+            LocalTime startSession = LocalTime.now();
+
+            try (BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(file))) {
+                while (totalReadLength < fileLength) {
+                    int len = in.read(buff);
+                    totalReadLength += len;
+                    partlyReadLength += len;
+                    out.write(buff, 0, len);
+
+                    if (LocalTime.now().getSecond() > 3 + timeLast.getSecond()) {
+                        System.out.println("average speed for client #" + curNumber + ": " + ((float) totalReadLength / (LocalTime.now().getSecond() - startSession.getSecond())) + " Bps");
+                        System.out.println("instantaneous speed for client #" + curNumber + ": " + ((float) partlyReadLength / (LocalTime.now().getSecond() - timeLast.getSecond())) + " Bps");
+                        timeLast = LocalTime.now();
+                        partlyReadLength = 0;
+                    }
+                }
+            }
+
+            oos.writeObject("success receiving");
+            oos.flush();
+
+            clientSocket.close();
+
+            System.out.println("connection with client #" + curNumber + " closed");
+            totalNumber--;
+            System.out.println("+----------------| DONE |----------------+");
+        } catch (ClassNotFoundException e) {
+            System.err.println(e.getMessage());
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+}
